a7786579c449aaa9ae031117fea5bbf2831c0fab
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-03-14 06:09:32 -0600

    start of programming

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString: connectionString }); 
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/gamePage'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
